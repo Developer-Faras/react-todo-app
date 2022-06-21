e99a1f2285808d419ff95c0aa03364a86632c959
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-06-21 10:25:42 +0600

    Complete Design

--- src/components/addtodo/AddTodo.js
+++ src/components/addtodo/AddTodo.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 // Import Styles
+import './addtodo.css';
 
 const AddTodo = () => {
     return (
         <div className='add-todo'>
-            add Todo
+            <form action="">
+                <div className="input-box"> 
+                    <input type="text" name="title" id="title" placeholder='Enter Your Title...' />
+                    <input type="text" name='desc' id='desc' placeholder='Enter Your Description...' />
+                </div>
+                <input type="submit" value="Add Todo" />
+            </form>
         </div>
     );
 }
